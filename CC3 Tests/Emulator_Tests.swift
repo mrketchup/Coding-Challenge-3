a721dafa4bde1bfd4d9af2275eca75ea539a2a2e
Matt Jones <matt.jones@willowtreeapps.com>
2016-05-19 20:25:17 -0400

    Add more emulator tests

--- CC3 Tests/Emulator_Tests.swift
+++ CC3 Tests/Emulator_Tests.swift
@@ -25,5 +25,61 @@ class Emulator_Tests: XCTestCase {
             XCTFail("\(error)")
         }
     }
+    
+    func testEmulatorSuccess() {
+        do {
+            let emulator = Emulator()
+            let executions = try emulator.execute([000, 222, 333, 444, 555, 666, 777, 888, 999, 100])
+            XCTAssertEqual(executions, 10)
+            XCTAssertEqual(emulator.registers, [0, 0, 2, 3, 0, 0, 0, 0, 0, 0])
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func testInvalidInstruction() {
+        do {
+            let emulator = Emulator()
+            try emulator.execute([1234])
+        } catch let error as Emulator.Error {
+            if case .InvalidInstruction(let instruction, let address) = error {
+                XCTAssertEqual(instruction, 1234)
+                XCTAssertEqual(address, 0)
+            } else {
+                XCTFail("\(error)")
+            }
+        } catch {
+            XCTFail("\(error)")
+        }
+        
+        do {
+            let emulator = Emulator()
+            try emulator.execute([111])
+        } catch let error as Emulator.Error {
+            if case .InvalidInstruction(let instruction, let address) = error {
+                XCTAssertEqual(instruction, 111)
+                XCTAssertEqual(address, 0)
+            } else {
+                XCTFail("\(error)")
+            }
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
+    
+    func testMemoryOutOfBounds() {
+        do {
+            let emulator = Emulator()
+            try emulator.execute([000])
+        } catch let error as Emulator.Error {
+            if case .MemoryOutOfBounds = error {
+                XCTAssertTrue(true)
+            } else {
+                XCTFail("\(error)")
+            }
+        } catch {
+            XCTFail("\(error)")
+        }
+    }
 
 }
